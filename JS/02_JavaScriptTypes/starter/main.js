49d1ca3f34207974f68edc4eb365612cbc8b2f73
Jemima Hsu <jemimahsu@yahoo.co.uk>
2023-08-23 15:53:13 +0100

    Added comments

--- JS/02_JavaScriptTypes/starter/main.js
+++ JS/02_JavaScriptTypes/starter/main.js
@@ -1,19 +1,23 @@
+// Number variable
 let numTest = 45.324568;
 console.log(numTest);
 
+// Truncate number
 let twoDecimalPoints = numTest.toFixed(2);
-console.log(twoDecimalPoints);
+console.log(twoDecimalPoints); // NB Turned number into string!
 
+// String 
 let stringTest = 'I am the very model of a modern major general';
 stringTest = stringTest.toUpperCase();
 let indexOfM = stringTest.indexOf('M');
-console.log(indexOfM);
+console.log(indexOfM); // Not found, returns -1
 
-let start = stringTest.indexOf("MODEL");
-let end = stringTest.lastIndexOf('MAJOR');
+let start = stringTest.indexOf("MODEL"); // position of first occurrence
+let end = stringTest.lastIndexOf('MAJOR'); // last occurrence in the string
 console.log(start);
-console.log(end);
+console.log(end); 
 
-let subStr = stringTest.substring(start, end);
+let subStr = stringTest.substring(start, end); // extract substring
 console.log(subStr);
-document.write("<p>" + subStr + "</p>");
+
+document.write("<p>" + subStr + "</p>"); // Renders on browser as HTML!
